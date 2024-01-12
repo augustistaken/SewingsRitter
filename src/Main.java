eb36e3952ac8ff003a96e163d43d78c82f53394a
BuildTools <unconfigured@null.spigotmc.org>
2024-01-12 20:26:25 +0100

    new

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         HolsDerGeier holsDerGeier = new HolsDerGeier();
         holsDerGeier.neueSpieler(new FiveOrRandom(), new SewingsRitter());
-        for(int i=0;i<1000;i++) {
+        for(int i=0;i<10;i++) {
             holsDerGeier.ganzesSpiel();
             holsDerGeier.naechstesSpiel();
         }
--- src/SewingsRitter.java
+++ src/SewingsRitter.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -7,24 +8,24 @@ public class SewingsRitter extends MyBot {
     private int gameNumber;
     private int wins;
     private Strategy myStrategy;
-    private int numOfRandomGames = 10000;
-
+    private int numOfRandomGames = 1000;
 
     public SewingsRitter() {
         super();
     }
+
     @Override
     public void reset() {
-        if(turnNumber == 15) {
+        if (turnNumber == 15) {
             turnNumber = 0;
 
             System.out.println("Game " + gameNumber + ": " + myPoints + " - " + hisPoints);
-            if(myPoints > hisPoints) {
+            if (myPoints > hisPoints) {
                 wins++;
             }
             gameNumber++;
-            System.out.println("Wins: " + wins + " out of "+ gameNumber);
-            if(gameNumber == 1) {
+            System.out.println("Wins: " + wins + " out of " + gameNumber);
+            if (gameNumber == 1) {
                 evaluation();
             } else {
                 ((MyBot) myStrategy).reset();
@@ -32,10 +33,11 @@ public class SewingsRitter extends MyBot {
         }
         super.reset();
     }
+
     @Override
     public int gibKarte(int nextCard) {
         int myCard;
-        if(gameNumber == 0)
+        if (gameNumber == 0)
             myCard = firstRun(nextCard);
         else
             myCard = myStrategy.giveCard(nextCard);
@@ -43,151 +45,78 @@ public class SewingsRitter extends MyBot {
         return myCard;
     }
 
-        public void evaluation() {
+    public void evaluation() {
         int attempts = 1;
-        List<int[]> attemptArray = new ArrayList<>();
-            int[] permArray;
-            while(attempts< 16) {
-                permArray = new int[15];
-                for (int i = 0; i < 15; i++)
-                    permArray[i] = -1;
-                int[] testArray = new int[15];
-                int index = 0;
-                for (int i = attempts % 15; i < 16; i++) {
-                    int[] winPoints = new int[15];
-                    while (index < 15) {
-                        for (int l = 0; l < 15; l++) {
-                            if (permArray[l] == -1)
-                                testArray[l] = -1;
-                            else
-                                testArray[l] = permArray[l];
+        int[] permArray;
+        first:
+        while (attempts < 16) {
+            permArray = new int[15];
+            Arrays.fill(permArray, -1); // Fill permArray with -1
+            int[] testArray = new int[15];
+            int index = 0;
+            int start = attempts % 15;
+            if (start == 0) start = 15;
+            for (int i = attempts % 15; i < 16; i++) {
+                int[] winPoints = new int[15];
+                while (index < 15) {
+                    for (int l = 0; l < 15; l++) {
+                        if (permArray[l] == -1)
+                            testArray[l] = -1;
+                        else
+                            testArray[l] = permArray[l];
+                    }
+                    testArray[index] = i;
+                    for (int k = 0; k < numOfRandomGames; k++) {
+                        for (int j = 0; j < 15; j++) {
+                            if (testArray[j] == -1) {
+                                boolean inTestArray;
+                                do {
+                                    int number = (int) (Math.random() * 15) + 1;
+                                    inTestArray = false;
+                                    for (int element : testArray) {
+                                        if (element == number) {
+                                            inTestArray = true;
+                                            break;
+                                        }
+                                    }
+                                    if (!inTestArray) {
+                                        testArray[j] = number;
+                                    }
+                                } while (inTestArray);
+                            }
+                        }
+
+                        Simulation simulation = new Simulation(specialCardsPlayed, new Last(Arrays.stream(testArray).boxed().collect(Collectors.toList())), new Last(hisCardsPlayed));
+                        int pointsWon = simulation.playGame();
+                        if (pointsWon > 35) {
+                            System.out.println(Arrays.toString(testArray));
+                            myStrategy = new Counter(Arrays.stream(testArray).boxed().collect(Collectors.toList()), specialCardsPlayed);
+                            break first;
+                        }
+                        winPoints[index] += pointsWon;
+                        for (int m = 0; m < 15; m++) {
+                            if (permArray[m] != testArray[m])
+                                testArray[m] = -1;
                         }
                         testArray[index] = i;
-                        for (int k = 0; k < numOfRandomGames; k++) {
-                            for (int j = 0; j < 15; j++) {
-                                if (testArray[j] == -1) {
-                                    boolean inTestArray;
-                                    do {
-                                        int number = (int) (Math.random() * 15) + 1;
-                                        inTestArray = false;
-                                        for (int element : testArray) {
-                                            if (element == number) {
-                                                inTestArray = true;
-                                                break;
-                                            }
-                                        }
-                                        if (!inTestArray) {
-                                            testArray[j] = number;
-                                        }
-                                    } while (inTestArray);
-                                }
-                            }
-
-                            Simulation simulation = new Simulation(specialCardsPlayed, new Last(Arrays.stream(testArray).boxed().collect(Collectors.toList())), new Last(hisCardsPlayed));
-                            winPoints[index] = winPoints[index] + simulation.playGame();
-                            testArray[index] = i;
-                        }
-                        index++;
                     }
-                    int j = 0;
-                    int[] winnerIndices = sortIndicesByArrayValues(winPoints);
-                    int winnerIndex = -1;
-                    for (int pos : winnerIndices) {
-                        if (permArray[pos] == -1) {
-                            winnerIndex = pos;
-                            break;
-                        }
-
-                    }
-                    permArray[winnerIndex] = i;
-                    index = 0;
+                    index++;
                 }
-                for(int i = 1; i< attempts % 15; i++) {
-                    int[] winPoints = new int[15];
-                    while (index < 15) {
-                        for (int l = 0; l < 15; l++) {
-                            if (permArray[l] == -1)
-                                testArray[l] = -1;
-                            else
-                                testArray[l] = permArray[l];
-                        }
-                        testArray[index] = i;
-                        for (int k = 0; k < numOfRandomGames; k++) {
-                            for (int j = 0; j < 15; j++) {
-                                if (testArray[j] == -1) {
-                                    boolean inTestArray;
-                                    do {
-                                        int number = (int) (Math.random() * 15) + 1;
-                                        inTestArray = false;
-                                        for (int element : testArray) {
-                                            if (element == number) {
-                                                inTestArray = true;
-                                                break;
-                                            }
-                                        }
-                                        if (!inTestArray) {
-                                            testArray[j] = number;
-                                        }
-                                    } while (inTestArray);
-                                }
-                            }
-
-                            Simulation simulation = new Simulation(specialCardsPlayed, new Last(Arrays.stream(testArray).boxed().collect(Collectors.toList())), new Last(hisCardsPlayed));
-                            winPoints[index] = winPoints[index] + simulation.playGame();
-                            testArray[index] = i;
-                        }
-                        index++;
+                int[] winnerIndices = sortIndicesByArrayValues(winPoints);
+                int winnerIndex = -1;
+                for (int pos : winnerIndices) {
+                    if (permArray[pos] == -1) {
+                        winnerIndex = pos;
+                        break;
                     }
-                    int j = 0;
-                    int[] winnerIndices = sortIndicesByArrayValues(winPoints);
-                    int winnerIndex = -1;
-                    for (int pos : winnerIndices) {
-                        if (permArray[pos] == -1) {
-                            winnerIndex = pos;
-                            break;
-                        }
 
-                    }
-                    permArray[winnerIndex] = i;
-                    index = 0;
                 }
-                attemptArray.add(permArray);
-                attempts++;
+                permArray[winnerIndex] = i;
+                index = 0;
             }
-            permArray = compareArrayPositions(attemptArray);
-            System.out.println(Arrays.toString(permArray));
-            System.out.println(Arrays.toString(specialCardsPlayed.toArray()));
-            myStrategy = new Counter(Arrays.stream(permArray).boxed().collect(Collectors.toList()), specialCardsPlayed);
+            attempts++;
         }
-
-    private int[] compareArrayPositions(List<int[]> grandArray) {
-        int[] permArray = new int[15];
-        int[][] frequencyCounter = new int[15][16]; // 15 positions, numbers 1 to 15
-
-        // Counting frequencies
-        for (int[] singleArray : grandArray) {
-            for (int i = 0; i < singleArray.length; i++) {
-                int number = singleArray[i];
-                if (number >= 1 && number <= 15) {
-                    frequencyCounter[i][number]++;
-                }
-            }
-        }
-
-        // Finding most frequent number for each position
-        for (int i = 0; i < 15; i++) {
-            int maxFrequency = 0;
-            int mostFrequentNumber = 0;
-            for (int j = 1; j <= 15; j++) {
-                if (frequencyCounter[i][j] > maxFrequency) {
-                    maxFrequency = frequencyCounter[i][j];
-                    mostFrequentNumber = j;
-                }
-            }
-            permArray[i] = mostFrequentNumber;
-        }
-
-        return permArray;
+        System.out.println(Arrays.toString(specialCardsPlayed.toArray()));
     }
 
     private int[] sortIndicesByArrayValues(int[] array) {
